7332f99b66a4a789077ddbfb7a2431ca97b1892b
roud <2553112258@qq.com>
2024-04-28 10:51:40 +0800

    php-demo新增运单附件查询接口示例代码

---
+++ src/backorder.php
@@ -0,0 +1,50 @@
+<?php
+    //====================================
+    // 运单附件查询示例代码（php）
+    //====================================
+
+    // 参数设置
+    $key = '';                             // 客户授权key
+    $secret = '';                          // 授权secret
+    list($msec, $sec) = explode(' ', microtime());
+    $t = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    // 当前时间戳
+    $param = array (                          //此处参数值为示例，请自行替换填充
+        'kuaidicom' => 'shunfeng',            //快递公司编码
+        'kuaidinum' => 'SF1234567',           //快递单号
+        'imgType' => 1,                       //快递100附件类型，默认1。1：回单
+        'partnerId' => '12345678',            //电子面单客户账户或月结账号
+        'partnerKey' => '12345678',           //电子面单密码
+        'phone' => '13088888888'              //寄件人手机号
+    );
+    
+    //请求参数
+    $post_data = array();
+    $post_data['param'] = json_encode($param, JSON_UNESCAPED_UNICODE);
+    $post_data['key'] = $key;
+    $post_data['t'] = $t;
+    $sign = md5($post_data['param'].$t.$key.$secret);
+    $post_data['sign'] = strtoupper($sign);
+    
+    $url = 'https://api.kuaidi100.com/label/order?method=backOrder';    // 运单附件查询接口请求地址
+    
+echo '请求参数：<br/><pre>';
+echo print_r($post_data);
+echo '</pre>';
+    
+    //发送post请求
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $result = curl_exec($ch);
+    $data = json_decode($result, true);
+
+echo '<br/><br/>返回数据：<br/><pre>';
+echo print_r($data);
+//echo var_dump($data);
+echo '</pre>';
+?>
