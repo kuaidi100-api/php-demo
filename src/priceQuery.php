7a1025b149e3c1e943c0b699f1e093688914d4ed
liguolin <guolin_li@kuaidi100.com>
2024-08-26 15:55:02 +0800

    新建价格查询接口

---
+++ src/priceQuery.php
@@ -0,0 +1,50 @@
+<?php
+    //====================================
+    // 地址解析示例代码（php）
+    //====================================
+
+    // 参数设置
+    $key = '';                             // 客户授权key
+    $secret = '';                          // 授权secret
+    list($msec, $sec) = explode(' ', microtime());
+    $t = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    // 当前时间戳
+    $param = array (                          //此处参数值为示例，请自行替换填充
+            'sendAddr'=>'深圳南山区',
+            'receiveAddr'=>'北京海淀区',
+            'companyName'=>'jd',
+            'weight'=>'1',
+            
+    );
+    
+    //请求参数
+    $post_data = array();
+    $post_data['param'] = json_encode($param, JSON_UNESCAPED_UNICODE);
+    $post_data['key'] = $key;
+    $post_data['t'] = $t;
+    $post_data['method'] = 'price';
+    $sign = md5($post_data['param'].$t.$key.$secret);
+    $post_data['sign'] = strtoupper($sign);
+    
+    $url = 'https://api.kuaidi100.com/label/order';    // 地址解析接口请求地址
+    
+echo '请求参数：<br/><pre>';
+echo print_r($post_data);
+echo '</pre>';
+    
+    //发送post请求
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $result = curl_exec($ch);
+    $data = json_decode($result, true);
+
+echo '<br/><br/>返回数据：<br/><pre>';
+echo print_r($data);
+//echo var_dump($data);
+echo '</pre>';
+?>
